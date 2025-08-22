b8d142640f9f6a3841a950a7945a2e1ca12a663a
SANGARE <sangarekassouminfo@gmail.com>
2025-08-22 19:27:45 +0000

    optimize

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-         Schema::defaultStringLength(191);
+        Schema::defaultStringLength(191);
 
         $this->app->booted(function () {
             try {
